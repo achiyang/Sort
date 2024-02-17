578d2fcec88d3cdd33860a637e8bc5de5fd63bd5
achiyang <sinhouse2@gmail.com>
2024-02-17 19:14:08 +0900

    ^Q^~

---
+++ heapsort.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <string.h>
+#include "heapsort.h"
+
+static void heapSortCore() {
+
+}
+
+void heapSort(void* arr, size_t num_elements, size_t size_element, compareFunc compare) {
+
+}
---
+++ heapsort.h
@@ -0,0 +1,8 @@
+#ifndef HEAPSORT_H
+#define HEAPSORT_H
+
+#include "sorttypes.h"
+
+void heapSort(void* arr, size_t num_elements, size_t size_element, compareFunc compare);
+
+#endif // HEAPSORT_H
