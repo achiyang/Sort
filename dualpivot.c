80333ffe121fecc29e1cfd427cf9f9030d5c72bb
achiyang <sinhouse2@gmail.com>
2024-02-20 03:33:16 +0900

    ^Q^~

---
+++ dualpivot.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dualpivot.h"
+
+void dualPivotQuickSort(void *arr, size_t num_elements, size_t size_element, compareFunc compare) {
+
+}
---
+++ dualpivot.h
@@ -0,0 +1,8 @@
+#ifndef DUALPIVOT_H
+#define DUALPIVOT_H
+
+#include "sorttypes.h"
+
+void dualPivotQuickSort(void *arr, size_t num_elements, size_t size_element, compareFunc compare);
+
+#endif // DUALPIVOT_H
